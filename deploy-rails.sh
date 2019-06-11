0a6f7a886c29eed14095c7e1cb5fc351dec5b7da
jonathanhuihaolaw <50609475+jonathanhuihaolaw@users.noreply.github.com>
2019-06-11 14:59:15 +0800

    Hello SSL

--- deploy-rails.sh
+++ deploy-rails.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+installSSL(){
+ sudo apt install certbot python-certbot-apache -t stretch-backports -y -qq
+ sudo certonly --standalone
+ sudo echo "@reboot /bin/bash -l -c 'cd /srv/www && sudo rails s -p 443 -b \"ssl://0.0.0.0?key=/etc/letsencrypt/live/[DOMAIN_NAME]/privkey.pem&cert=/etc/letsencrypt/live/[DOMAIN_NAME]/fullchain.pem\"'" >> startcron
+ sudo crontab startcron
+ sudo rm startcron
+ sudo apt autoremove
+ sudo rm -rf /var/apache2
+}
+
 echo "Setting up required packages for Ruby on Rails"
 echo "Creating directory for hosting at /srv/www"
 sudo mkdir /srv/www
@@ -19,7 +30,18 @@ echo "Installing Rails 4.2.1"
 echo "This process will take longer than usual"
 sudo gem install rails -v 4.2.1
 echo "Now, let rails serve public traffic on reboot"
-echo "Setting to start HTTP server at /srv/www/*"
+echo "Setting to start HTTP server at /srv/www/"
 sudo echo "@reboot /bin/bash -l -c 'cd /srv/www && sudo rails s -p 80 -b 0.0.0.0'" >> startcron
 sudo crontab startcron
 sudo rm startcron
+echo -e "Installation done"
+echo " - "
+
+while true; do
+    read -p "Do you want to setup SSL? y/n " yn
+    case $yn in
+        [Yy]* ) installSSL; break;;
+        [Nn]* ) exit;;
+        * ) echo -e "Please answer y or n.";;
+    esac
+done
